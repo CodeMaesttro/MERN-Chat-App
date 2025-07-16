39e41faef0ee300b9775647f3172aa739b515a45
CodeMaesttro <168296043+CodeMaesttro@users.noreply.github.com>
2025-07-16 06:58:01 -0700

    auth route update

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { signUp, signIn, logout } from '../controllers/auth.controllers.js'; // ✅ import logout
 
+
 const authRouter = express.Router();
 
 // Routes
-authRouter.post('/sign-up', signUp);
+authRouter.post('/sign-up', signUp); 
 authRouter.post('/sign-in', signIn);
 authRouter.post('/logout', logout); // ✅ logout route
 
